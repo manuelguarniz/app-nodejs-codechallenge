66f66ccbaf96ce8ac8427aa456eccbc4d619ec34
Manuel Guarniz <cruzemg95@gmail.com>
2025-04-24 13:42:13 -0500

    fix: fixed kafka config for new algorithm to partition

--- ms-transaction-api/src/configs/kafka.config.ts
+++ ms-transaction-api/src/configs/kafka.config.ts
@@ -1,8 +1,10 @@
-import { Kafka } from 'kafkajs';
+import { Kafka, Partitioners } from 'kafkajs';
 
 export const kafka = new Kafka({
   clientId: 'appid-transaction-api',
   brokers: ['localhost:9092'],
 });
 
-export const producer = kafka.producer();
+export const producer = kafka.producer({
+  createPartitioner: Partitioners.LegacyPartitioner,
+});
